527f001fd46729b31b39969428b4e03ec921ddf2
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-10-22 11:33:09 -0300

    feat: create /users

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,14 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).send();
 });
 
+app.get('/users', (req: Request, res: Response) => {
+    const users = [
+        {
+            name: 'John Doe',
+            email: 'johndoe@email.com'
+        }
+    ]
+    res.status(200).json(users);
+});
+
 export default app;
