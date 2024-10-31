78044dffdf5fdaa06b4f8c883f5553c59779a9da
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-10-30 21:30:51 -0300

    feat: create route account

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -4,4 +4,7 @@ module.exports = (app: Express) => {
     app.route('/users')
         .get(app.routes.users.findAll)
         .post(app.routes.users.create);
+
+    app.route('/accounts')
+        .post(app.routes.accounts.create);
 }
