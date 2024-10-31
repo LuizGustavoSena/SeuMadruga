c7d34e386a772479b3f4ce944d4a222f64dfde61
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-10-30 21:32:19 -0300

    test: create test create a successful account

---
+++ test/routes/accounts.test.ts
@@ -0,0 +1,35 @@
+import app from '@src/app';
+import supertest from 'supertest';
+
+const URL = '/accounts';
+const URL_USERS = '/users';
+
+const request = supertest(app);
+
+const USER = {
+    id: undefined,
+    name: 'Walter Mitty',
+    email: `${Date.now()}@email.com`,
+    password: '12354'
+}
+
+describe('Accounts', () => {
+    beforeAll(async () => {
+        const response = await request.post(URL_USERS).send(USER);
+
+        USER.id = response.body.id;
+    });
+
+    test('Should be create a successful account', async () => {
+        const account = {
+            name: 'Acc 1',
+            user_id: USER.id
+        }
+
+        const response = await request.post(URL).send(account);
+
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe(account.name);
+    })
+
+})
