85518b2524f9d5d3c396c216f5abd6de45dc8226
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-11-04 19:16:44 -0300

    feat: create a signin

---
+++ src/services/auth.ts
@@ -0,0 +1,28 @@
+import { SigninParams, SigninResponse } from "@src/domain/models/auth";
+import 'dotenv/config';
+import UserService from "./user";
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jwt-simple');
+
+export default class AuthService {
+    constructor(
+        private readonly userService: UserService
+    ) { };
+
+    async signin(params: SigninParams): Promise<SigninResponse> {
+        const user = await this.userService.findByEmail(params.email);
+
+        if (!user || !bcrypt.compareSync(params.password, user.password))
+            throw new Error('Usuário não encontrado');
+
+        const response = {
+            id: user.id,
+            email: user.email,
+            name: user.name
+        }
+
+        const token = jwt.encode(response, process.env.SECRET);
+
+        return { token };
+    }
+}
