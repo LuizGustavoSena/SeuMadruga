b3afe63fe8ddec049bc6b3be77d7ef79e7143cfe
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-11-25 18:52:35 -0300

    feat: create a security with request without token to any endpoint

---
+++ src/config/passport.ts
@@ -0,0 +1,36 @@
+import UserService from '@src/services/user';
+import 'dotenv/config';
+import { Express } from "express";
+
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+
+const userService = new UserService();
+
+module.exports = (app: Express) => {
+    const params = {
+        secretOrKey: process.env.SECRET,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt')
+    };
+
+    const strategy = new Strategy(params, async (payload: any, done: any) => {
+        try {
+            const user = await userService.findByEmail(payload.email);
+
+            if (user) {
+                done(null, { ...payload });
+                return;
+            }
+
+            done(null, false);
+        } catch (error) {
+            done(error, false);
+        }
+    });
+
+    passport.use(strategy);
+
+    return {
+        authenticate: () => passport.authenticate('jwt', { session: false })
+    }
+}
