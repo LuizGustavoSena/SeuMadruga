b1bae22897c90c1c20b994915fe495468deeec3a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-11-25 18:52:54 -0300

    feat: apply security in all routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,18 +1,19 @@
-import { Express } from "express";
-
-module.exports = (app: Express) => {
+module.exports = (app: any) => {
     app.route('/auth/signin')
         .post(app.routes.auth.signin);
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .get(app.routes.users.findAll)
         .post(app.routes.users.create);
 
     app.route('/accounts')
+        .all(app.config.passport.authenticate())
         .get(app.routes.accounts.getAll)
         .post(app.routes.accounts.create);
 
     app.route('/accounts/:id')
+        .all(app.config.passport.authenticate())
         .get(app.routes.accounts.getById)
         .put(app.routes.accounts.update)
         .delete(app.routes.accounts.deleteById)
