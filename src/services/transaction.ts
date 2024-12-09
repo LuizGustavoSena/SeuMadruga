5d89bf87f5a544b9edbe277403701e48ace8131d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-12-09 17:35:27 -0300

    feat: create transactionService

---
+++ src/services/transaction.ts
@@ -0,0 +1,27 @@
+import { CreateProps, CreateResponse, FindProps, FindResponse } from "@src/domain/models/transaction";
+import knex from 'knex';
+import config from "../../knexfile";
+
+const db = knex(config);
+
+export default class TransactionService {
+    tableName = 'transactions';
+    constructor() { }
+
+    async find(params: FindProps): Promise<FindResponse[]> {
+        const response = await db(this.tableName)
+            .join('accounts', 'accounts.id', 'acc_id')
+            .where(params.filter ?? {})
+            .andWhere('accounts.user_id', '=', params.user_id)
+            .select();
+
+        return response;
+    }
+
+    async create(params: CreateProps): Promise<CreateResponse> {
+        const response = await db(this.tableName)
+            .insert({ ...params, date: new Date() }, '*');
+
+        return response[0];
+    }
+};
