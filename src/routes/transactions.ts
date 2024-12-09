d78b08d60d0ef2c7de639fcf3df1e3b5bad1451f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-12-09 18:08:01 -0300

    feat: create router create transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,5 +1,7 @@
+import Validation from '@src/domain/validations';
 import TransactionService from '@src/services/transaction';
 import express, { Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 const transactionService = new TransactionService();
 
@@ -15,6 +17,20 @@ module.exports = () => {
             res.status(response.length > 0 ? 200 : 204).send(response);
         } catch (error: any) {
             res.status(400).send({ error: error.message });
+        }
+    });
+
+    router.post('/', async (req: Request, res: Response) => {
+        try {
+            Validation.createTransaction(req.body);
+
+            const response = await transactionService.create({ ...req.body, acc_id: req.user.id });
+
+            res.status(201).send(response);
+        } catch (error: any) {
+            let message = error instanceof ZodError ? `${error.errors.map(el => el.message).join(', ')}` : error.message;
+
+            res.status(400).json({ error: message });
         }
     });
 
