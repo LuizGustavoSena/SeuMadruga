a9247fa5f1029f5323d3409e0801e1bfd4fed4e6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-12-21 19:09:47 -0300

    feat: create transfer route

--- src/routes/transfers.ts
+++ src/routes/transfers.ts
@@ -1,5 +1,7 @@
+import Validation from '@src/domain/validations';
 import TransferService from '@src/services/transfer';
 import express, { Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 const transferService = new TransferService();
 
@@ -13,6 +15,20 @@ module.exports = () => {
             res.status(response.length > 0 ? 200 : 204).send(response);
         } catch (error: any) {
             res.status(400).send({ error: error.message });
+        }
+    });
+
+    router.post('/', async (req: Request, res: Response) => {
+        try {
+            Validation.createTransfer(req.body);
+
+            const response = await transferService.create({ ...req.body, user_id: req.user.id });
+
+            res.status(201).send(response);
+        } catch (error: any) {
+            let message = error instanceof ZodError ? `${error.errors.map(el => el.message).join(', ')}` : error.message;
+
+            res.status(400).json({ error: message });
         }
     });
 
