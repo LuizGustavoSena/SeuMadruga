3ab4c9870c67efd66ff5442d792e4be72e5e092b
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-12-21 19:09:53 -0300

    test: create transfer route

--- test/routes/transfer.test.ts
+++ test/routes/transfer.test.ts
@@ -1,4 +1,5 @@
 import app from '@src/app';
+import { CreateTransfer } from '@src/domain/models/transfer';
 import AuthService from '@src/services/auth';
 import UserService from '@src/services/user';
 import supertest from 'supertest';
@@ -27,5 +28,23 @@ describe('Auth', () => {
         expect(response.status).toBe(200);
         expect(response.body).toHaveLength(1);
         expect(response.body[0].description).toBe('Transfer 1');
+    });
+
+    test('Should be create successful transfer', async () => {
+        const params: Partial<CreateTransfer> = {
+            acc_ori_id: 10003,
+            acc_dest_id: 10004,
+            ammount: 100,
+            description: 'Successful transfer'
+        };
+
+        const response = await request.post(URL)
+            .set('authorization', `JWT ${token}`)
+            .send(params);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('description');
+        expect(response.body.description).toBe(params.description);
     });
 })
