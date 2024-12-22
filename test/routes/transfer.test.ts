8ba8731073578bb819a1dbb74c12094f35b09d34
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-12-22 07:42:47 -0300

    test: complement create transfer

--- test/routes/transfer.test.ts
+++ test/routes/transfer.test.ts
@@ -1,10 +1,14 @@
 import app from '@src/app';
+import { Type } from '@src/domain/models/transaction';
 import { CreateTransfer } from '@src/domain/models/transfer';
 import AuthService from '@src/services/auth';
 import TransactionService from '@src/services/transaction';
 import UserService from '@src/services/user';
+import knex from 'knex';
 import supertest from 'supertest';
+import config from "../../knexfile";
 
+const db = knex(config);
 const URL = '/v1/transfers';
 var token = '';
 
@@ -12,8 +16,10 @@ const transactionService = new TransactionService();
 
 const request = supertest(app);
 
-describe('Auth', () => {
+describe('Transfer', () => {
     beforeAll(async () => {
+        await db.seed.run();
+
         const authService = new AuthService(new UserService());
 
         const response = await authService.signin({
@@ -61,8 +67,12 @@ describe('Auth', () => {
         expect(transactions[0].description).toBe(`Transfer to account_id: ${params.acc_dest_id}`);
         expect(transactions[0].ammount).toBe(`-${params.ammount}.00`);
         expect(transactions[0].acc_id).toBe(params.acc_ori_id);
+        expect(transactions[0].type).toBe(Type.OUTPUT);
+        expect(transactions[0].transfer_id).toBe(response.body.id);
         expect(transactions[1].description).toBe(`Transfer from account_id: ${params.acc_ori_id}`);
         expect(transactions[1].ammount).toBe(`${params.ammount}.00`);
         expect(transactions[1].acc_id).toBe(params.acc_dest_id);
+        expect(transactions[1].type).toBe(Type.INPUT);
+        expect(transactions[1].transfer_id).toBe(response.body.id);
     });
 })
