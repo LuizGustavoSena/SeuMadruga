6bfb59bfc31becc67bf1aee2fb163ac5363e5b15
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-01-02 11:09:45 -0300

    feat: remove user route

--- src/config/router.ts
+++ src/config/router.ts
@@ -5,7 +5,6 @@ module.exports = (app: any) => {
 
     app.use('/auth', app.routes.auth);
 
-    protectedRouter.use('/users', app.routes.users);
     protectedRouter.use('/accounts', app.routes.accounts);
     protectedRouter.use('/transactions', app.routes.transactions);
     protectedRouter.use('/transfers', app.routes.transfers);
