9c3c1140d73f6677fd1c74460ed207122f3e5e25
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-01-20 18:56:36 -0300

    feat: knexDatabase class

---
+++ src/infrastructure/database/knex.ts
@@ -0,0 +1,40 @@
+import knex from 'knex';
+import config from "../../../knexfile";
+import { Database, ParamsUpdateDatabase } from "../models/database";
+
+export default class KnexDatabase implements Database {
+    db = knex(config);
+    tableName;
+
+    constructor(tableName: string) {
+        this.tableName = tableName
+    }
+
+    async create<T, R>(params: T): Promise<R> {
+        const response = await this.db(this.tableName).insert(params, '*');
+
+        return response[0];
+    }
+
+    async getAll<T>(): Promise<T> {
+        const response = await this.db.select('*').from(this.tableName);
+
+        return response as T;
+    }
+
+    async getByFIlter<T>(filter: any): Promise<T> {
+        const response = await this.db.select('*').from(this.tableName).where(filter);
+
+        return response as T;
+    }
+
+    async update<T>(params: ParamsUpdateDatabase<T>): Promise<T> {
+        const response = await this.db(this.tableName).where({ id: params.id }).update(params.data, ['*']);
+
+        return response[0];
+    }
+
+    async deleteById(id: number): Promise<void> {
+        await this.db(this.tableName).where({ id }).del();
+    }
+}
