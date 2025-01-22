1e078155e961d3f30370c4a86d4698fc3bdee252
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-01-22 08:33:33 -0300

    fix: adjust auth class for bcryptEncrypt

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,5 @@
 import { SigninParams, SigninResponse } from "@src/domain/models/auth";
+import { Encrypt } from "@src/infrastructure/models/encrypt";
 import 'dotenv/config';
 import UserService from "./user";
 const bcrypt = require('bcryptjs');
@@ -6,13 +7,19 @@ const jwt = require('jwt-simple');
 
 export default class AuthService {
     constructor(
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private readonly encrypt: Encrypt,
     ) { };
 
     async signin(params: SigninParams): Promise<SigninResponse> {
         const user = await this.userService.findByEmail(params.email);
 
-        if (!user || !bcrypt.compareSync(params.password, user.password))
+        if (!user)
+            throw new Error('Usuário não encontrado');
+
+        const samePassword = await this.encrypt.compare(params.password, user.password);
+
+        if (!samePassword)
             throw new Error('Usuário não encontrado');
 
         const response = {
