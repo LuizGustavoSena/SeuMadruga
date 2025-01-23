866dc95283a4abfc5c98e98a65f02354531468b4
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-01-23 19:39:59 -0300

    feat; create a BalanceknexDatabase class

---
+++ src/infrastructure/database/specific/balanceKnex.ts
@@ -0,0 +1,24 @@
+import { getBalanceByUserIdResponse } from "@src/domain/models/balance";
+import { BalanceDatabase } from "@src/infrastructure/models/database";
+import knex from 'knex';
+import config from "../../../../knexfile";
+
+export default class BalanceKnexDatabase implements BalanceDatabase {
+    db = knex(config);
+    tableTransactionName = 'transactions';
+    tableAccountName = 'accounts';
+
+    constructor() { };
+
+    async getBalanceByUserId(userId: number): Promise<getBalanceByUserIdResponse[]> {
+        const response = await this.db(`${this.tableTransactionName} as t`).sum('ammount')
+            .join(`${this.tableAccountName} as acc`, 'acc.id', '=', 't.acc_id')
+            .where({ user_id: userId, status: true })
+            .where('date', '<=', new Date())
+            .select('acc.id')
+            .groupBy('acc.id')
+            .orderBy('acc.id');
+
+        return response as getBalanceByUserIdResponse[];
+    }
+}
