b014c651c09bc9544947b4c2d0dc3fbe6f1d519a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-01-23 19:40:17 -0300

    fix: adjust class to BalanceKnexDatabase class

--- src/services/balance.ts
+++ src/services/balance.ts
@@ -1,24 +1,14 @@
 import { getBalanceByUserIdResponse } from '@src/domain/models/balance';
-import knex from 'knex';
-import config from "../../knexfile";
-
-const db = knex(config);
+import { BalanceDatabase } from '@src/infrastructure/models/database';
 
 export default class BalanceService {
-    tableTransactionName = 'transactions';
-    tableAccountName = 'accounts';
-
-    constructor() { };
+    constructor(
+        private readonly db: BalanceDatabase
+    ) { };
 
     async getBalanceByUserId(userId: number): Promise<getBalanceByUserIdResponse[]> {
-        const response = await db(`${this.tableTransactionName} as t`).sum('ammount')
-            .join(`${this.tableAccountName} as acc`, 'acc.id', '=', 't.acc_id')
-            .where({ user_id: userId, status: true })
-            .where('date', '<=', new Date())
-            .select('acc.id')
-            .groupBy('acc.id')
-            .orderBy('acc.id');
+        const response = await this.db.getBalanceByUserId(userId);
 
-        return response as getBalanceByUserIdResponse[];
+        return response;
     }
 }
