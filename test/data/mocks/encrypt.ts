7f1ba71f73e7248658a70fd82925c5612f690c63
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-12 18:37:09 -0300

    feat: create a EncryptSpy class

---
+++ test/data/mocks/encrypt.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker';
+import { Encrypt } from "../protocols/encrypt/encrypt";
+
+class EncryptSpy implements Encrypt {
+    encryptText: string = faker.string.uuid();
+
+    constructor() { };
+
+    async create(text: string): Promise<string> {
+        return `${this.encryptText}${text}`;
+    }
+
+    async compare(encryptText: string, text: string): Promise<boolean> {
+        return encryptText.includes(`${this.encryptText}${text}`);
+    }
+}
+
+export default class MakeEncryptSpy {
+    private static instance: EncryptSpy;
+
+    constructor() { };
+
+    getInstance() {
+        if (!MakeEncryptSpy.instance)
+            MakeEncryptSpy.instance = new EncryptSpy();
+
+        return MakeEncryptSpy.instance;
+    }
+}
