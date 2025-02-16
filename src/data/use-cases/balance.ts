f46b9d0f5c43bf046326b42c77cbc79a2786a1a7
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-15 22:16:37 -0300

    feat: implements Balance interface

--- src/data/use-cases/balance.ts
+++ src/data/use-cases/balance.ts
@@ -1,7 +1,8 @@
 import { BalanceDatabase } from '@src/data/protocols/database/specif/balanceDatabase';
 import { getBalanceByUserIdResponse } from '@src/domain/models/balance';
+import { Balance } from '@src/domain/use-cases/balance';
 
-export default class BalanceService {
+export default class BalanceService implements Balance {
     constructor(
         private readonly db: BalanceDatabase
     ) { };
