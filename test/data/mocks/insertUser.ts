893bc09a9fe7a40392c71d86c4ae4740e71f28e7
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-16 11:23:16 -0300

    fix: alter makeUser props

--- test/data/mocks/insertUser.ts
+++ test/data/mocks/insertUser.ts
@@ -1,10 +1,10 @@
 import { faker } from "@faker-js/faker/."
 import { UserProps } from "@src/domain/models/user"
 
-export const makeUser = (): UserProps  => {
+export const makeUser = (params?: Partial<UserProps>): UserProps => {
     return {
-        email: faker.internet.email(),
-        name: faker.person.fullName(),
-        password: faker.internet.password()
+        email: params?.email ? params?.email : faker.internet.email(),
+        name: params?.name ? params?.name : faker.person.fullName(),
+        password: params?.password ? params?.password : faker.internet.password()
     }
 }
