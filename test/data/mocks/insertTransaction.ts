0056cac0f1c5e8b6e8566b710afd8fe2ca65bc78
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-18 17:10:47 -0300

    test: create makeTransaction

---
+++ test/data/mocks/insertTransaction.ts
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker/.";
+import { TransactionProps, Type } from "@src/domain/models/transaction";
+
+export const makeTransaction = (params?: Partial<TransactionProps>): TransactionProps => {
+    return {
+        acc_id: params?.acc_id ? params.acc_id : faker.number.int(),
+        ammount: params?.ammount ? params.ammount : faker.number.float(),
+        description: params?.description ? params.description : faker.string.sample(),
+        status: params?.status ? params.status : Math.random() < 0.5,
+        type: params?.type ? params.type : Math.random() < 0.5 ? Type.INPUT : Type.OUTPUT,
+        date: params?.date ? params.date : faker.date.past(),
+        transfer_id: params?.transfer_id ? params.transfer_id : faker.number.int()
+    }
+}
