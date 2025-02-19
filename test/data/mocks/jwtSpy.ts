4f5feaed4887bbd8c36e4984d13e75b03d098fb6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-19 18:20:11 -0300

    test: adjust encode method JwtSpy

--- test/data/mocks/jwtSpy.ts
+++ test/data/mocks/jwtSpy.ts
@@ -1,9 +1,13 @@
 import { EncodeProps, Jwt } from "../protocols/jwt/jwt";
 
 export default class JwtSpy implements Jwt {
+    params: EncodeProps;
+    response: string;
     constructor() { };
 
     encode(params: EncodeProps): string {
-        return JSON.stringify(params);
+        this.params = params;
+
+        return this.response;
     }
 }
