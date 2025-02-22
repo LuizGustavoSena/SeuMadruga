3214f19695615638fd9e12399232f28aa21e5206
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-22 17:45:12 -0300

    test: Should successful get transaction by id

--- test/data/use-cases/transaction.test.ts
+++ test/data/use-cases/transaction.test.ts
@@ -36,8 +36,6 @@ describe('Transaction', () => {
 
         const response = await sut.create(transaction);
 
-        console.log(database.content)
-
         expect(response.acc_id).toBe(transaction.acc_id);
         expect(response.ammount).toBe(transaction.ammount);
         expect(response.description).toBe(transaction.description);
@@ -48,145 +46,21 @@ describe('Transaction', () => {
         expect(response.date).toBe(database.content[0].date);
     });
 
-    // test('Should be error create a transaction with negative ammount on Input type', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: -100,
-    //             description: 'Successful transaction',
-    //             type: Type.INPUT,
-    //             acc_id: ACCOUNTS[0].id
-    //         });
+    test('Should successful get transaction by id', async () => {
+        const { sut, database } = makeSut();
 
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('deve ser positivo');
-    // });
+        const id = faker.number.int();
+        const transaction = makeTransaction()
 
-    // test('Should be error create a transaction with positive ammount on Output type', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             description: 'Successful transaction',
-    //             type: Type.OUTPUT,
-    //             acc_id: ACCOUNTS[0].id
-    //         });
+        database.content.push({
+            ...transaction,
+            id
+        });
 
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('deve ser negativo');
-    // });
+        const response = await sut.findById(id);
 
-    // test('Should be error create a transaction without description', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             type: Type.OUTPUT,
-    //             acc_id: ACCOUNTS[0].id
-    //         });
-
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('description deve ser preenchido');
-    // });
-
-    // test('Should be error create a transaction without ammount', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             description: 'Error transaction without ammount',
-    //             type: Type.OUTPUT,
-    //             acc_id: ACCOUNTS[0].id
-    //         });
-
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('ammount deve ser preenchido');
-    // });
-
-    // test('Should be error create a transaction without type or with incorrect type', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             description: 'Error transaction without type or with incorrect type',
-    //             acc_id: ACCOUNTS[0].id
-    //         });
-
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('type inválido');
-    // });
-
-    // test('Should be error create a transaction without acc_id', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             description: 'Error transaction without type or with incorrect type',
-    //             type: Type.INPUT
-    //         });
-
-    //     expect(response.status).toBe(400);
-    //     expect(response.body.error).toContain('acc_id deve ser preenchido');
-    // });
-
-    // test('Should be error create a transaction with acc_id from another user', async () => {
-    //     const response = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             description: 'Error transaction without type or with incorrect type',
-    //             type: Type.INPUT,
-    //             acc_id: ACCOUNTS[1].id
-    //         });
-
-    //     expect(response.status).toBe(403);
-    //     expect(response.body.error).toContain('Conta pertence a outro usuário');
-    // });
-
-    // test('Should don`t list another transaction account`s', async () => {
-    //     const description = 'Don`t list another transaction account`s';
-
-    //     await transactionService.create({
-    //         acc_id: ACCOUNTS[0].id ?? 0,
-    //         ammount: 100,
-    //         description,
-    //         type: Type.INPUT,
-    //         status: true
-    //     });
-
-    //     await transactionService.create({
-    //         acc_id: ACCOUNTS[1].id ?? 0,
-    //         ammount: 100,
-    //         description,
-    //         type: Type.INPUT,
-    //         status: true
-    //     });
-
-    //     const response = await request.get(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`);
-
-    //     const filter = response.body.filter((el: FindResponse) => el.description === description);
-
-    //     expect(response.status).toBe(200);
-    //     expect(filter).toHaveLength(1);
-    //     expect(filter[0].acc_id).toBe(ACCOUNTS[0].id);
-    // });
-
-    // test('Should successful get transaction by id', async () => {
-    //     const transaction = await request.post(URL_TRANSACTION)
-    //         .set('authorization', `JWT ${USERS[0].token}`)
-    //         .send({
-    //             ammount: 100,
-    //             description: 'Successful transaction',
-    //             type: Type.INPUT,
-    //             acc_id: ACCOUNTS[0].id
-    //         });
-
-    //     const response = await request.get(`${URL_TRANSACTION}/${transaction.body.id}`)
-    //         .set('authorization', `JWT ${USERS[0].token}`);
-
-    //     expect(response.status).toBe(200);
-    //     expect(response.body.id).toBe(transaction.body.id);
-    // });
+        expect(response).toEqual({ ...transaction, id });
+    });
 
     // test('Should error get transaction by another user', async () => {
     //     const transaction = await request.post(URL_TRANSACTION)
