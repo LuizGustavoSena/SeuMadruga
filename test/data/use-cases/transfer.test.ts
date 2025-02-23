8705d95550dcfe4db24dde8345b5e18e54f9b7f1
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-23 18:49:02 -0300

    test: Should be successful create transfer

--- test/data/use-cases/transfer.test.ts
+++ test/data/use-cases/transfer.test.ts
@@ -1,5 +1,6 @@
+import TransferService from '@src/data/use-cases/transfer';
+import { makeTransfer } from '../mocks/insertTransfer';
 import TransferDatabaseSpy from '../mocks/transferDatabaseSpy';
-import TransferService from './transfer';
 
 type Props = {
     sut: TransferService;
@@ -17,5 +18,20 @@ const makeSut = (): Props => {
 }
 
 describe('Transfer', () => {
+    test('Should be successful create transfer', async () => {
+        const { sut, database } = makeSut();
 
+        const transfer = makeTransfer();
+
+        const response = await sut.create(transfer);
+
+        expect(database.content[0].description).toBe(transfer.description);
+        expect(database.content[0].user_id).toBe(transfer.user_id);
+        expect(database.params.acc_dest_id).toBe(transfer.acc_dest_id);
+        expect(database.params.acc_ori_id).toBe(transfer.acc_ori_id);
+        expect(database.params.ammount).toBe(transfer.ammount);
+        expect(database.params.date).not.toBe(transfer.date);
+        expect(database.params.id).toBe(database.content[0].id);
+        expect(response).toEqual(database.content[0]);
+    });
 });
