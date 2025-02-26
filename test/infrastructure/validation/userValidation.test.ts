a6c64e49a27bc7f04395beb5f855909687d18c1d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-26 18:50:19 -0300

    test: Should be error when create user without name

---
+++ test/infrastructure/validation/userValidation.test.ts
@@ -0,0 +1,27 @@
+import UserService from "@src/data/use-cases/user";
+import { ValidationError } from "@src/domain/error/validationError";
+import { UserRequiredError } from "@src/domain/validations/user";
+import UserValidationZod from "@src/infrastructure/validation/zod/userValidationZod";
+import DatabaseSpy from "@test/data/mocks/databaseSpy";
+import { EncryptSpy } from "@test/data/mocks/encryptSpy";
+import { makeUser } from "@test/data/mocks/insertUser";
+
+const makeSut = (): UserService => {
+    const sut = new UserService(new DatabaseSpy(), new EncryptSpy(), new UserValidationZod())
+
+    return sut;
+}
+
+describe('UserValidation', () => {
+    test('Should be error when create user without name', async () => {
+        const sut = makeSut();
+
+        const request = makeUser();
+        // @ts-expect-error
+        delete request.name;
+
+        const promise = sut.save(request);
+
+        await expect(promise).rejects.toThrow(new ValidationError(UserRequiredError.NAME));
+    })
+});
