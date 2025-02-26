b1db2d48fd3c5e32a2b5d103bd1a2713f94e9311
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-02-26 18:49:45 -0300

    feat: adjust MakeUserService to MakeUserValidation

--- src/main/factories/use-cases/makeUserService.ts
+++ src/main/factories/use-cases/makeUserService.ts
@@ -1,6 +1,7 @@
 import UserService from "@src/data/use-cases/user";
 import MakeDatabase from "../database/makeDatabase";
 import MakeEncrypt from "../encrypt/makeEncrypt";
+import MakeUserValidation from "../validation/user";
 
 export default class MakeUserService {
     private static instance: UserService;
@@ -8,8 +9,13 @@ export default class MakeUserService {
     private constructor() { }
 
     static getInstance(): UserService {
-        if (!MakeUserService.instance)
-            MakeUserService.instance = new UserService(MakeDatabase.getInstance('users'), MakeEncrypt.getInstance());
+        if (!MakeUserService.instance) {
+            const database = MakeDatabase.getInstance('users');
+            const encrypt = MakeEncrypt.getInstance();
+            const validation = MakeUserValidation.getInstance();
+
+            MakeUserService.instance = new UserService(database, encrypt, validation);
+        }
 
         return MakeUserService.instance;
     }
