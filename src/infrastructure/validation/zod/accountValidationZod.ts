259dc4e81bb71c5beee1ba1b93ca3bb9d9e02aba
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-01 23:25:09 -0300

    fix: adjust AccountValidationZod class to new method interface

--- src/infrastructure/validation/zod/accountValidationZod.ts
+++ src/infrastructure/validation/zod/accountValidationZod.ts
@@ -1,15 +1,26 @@
 import { ValidationError } from "@src/domain/error/validationError";
-import { CreateProps } from "@src/domain/models/account";
-import { AccountValidation } from "@src/domain/validations/account";
+import { CreateProps, UpdateParams } from "@src/domain/models/account";
+import { AccountRequiredError, AccountValidation } from "@src/domain/validations/account";
 import { z, ZodError } from "zod";
 
 export default class AccountValidationZod implements AccountValidation {
     validation = z.object({
-        name: z.string({ required_error: 'Campo name deve ser preenchido' })
+        name: z.string({ required_error: AccountRequiredError.NAME })
     });
-    constructor() { };
 
-    createOrUpdateAccount(params: CreateProps): void {
+    constructor() { }
+
+    create(params: CreateProps): void {
+        try {
+            this.validation.parse(params)
+        } catch (error: any) {
+            if (!(error instanceof ZodError)) return;
+
+            throw new ValidationError(error.errors.map(el => el.message).join(', '))
+        }
+    }
+
+    update(params: UpdateParams): void {
         try {
             this.validation.parse(params)
         } catch (error: any) {
