6c72a84f0e13c582b1e6fbbdfc888eadb0dcf97c
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-02 21:19:27 -0300

    test: AuthValidation - Should be error when signin without email

---
+++ test/infrastructure/validation/authValidation.test.ts
@@ -0,0 +1,35 @@
+import { faker } from "@faker-js/faker/.";
+import AuthService from "@src/data/use-cases/auth";
+import UserService from "@src/data/use-cases/user";
+import { ValidationError } from "@src/domain/error/validationError";
+import { AuthRequiredError } from "@src/domain/validations/auth";
+import AuthValidationZod from "@src/infrastructure/validation/zod/authValidationZod";
+import UserValidationZod from "@src/infrastructure/validation/zod/userValidationZod";
+import DatabaseSpy from "@test/data/mocks/databaseSpy";
+import MakeEncryptSpy from "@test/data/mocks/encryptSpy";
+import JwtSpy from "@test/data/mocks/jwtSpy";
+
+const makeSut = (): AuthService => {
+    const encrypt = MakeEncryptSpy.getInstance();
+    const sut = new AuthService(
+        new UserService(new DatabaseSpy(), encrypt, new UserValidationZod()),
+        encrypt,
+        new JwtSpy(),
+        new AuthValidationZod())
+        ;
+
+    return sut;
+}
+
+describe('AuthValidation', () => {
+    test('Should be error when signin without email', async () => {
+        const sut = makeSut();
+
+        const request = { password: faker.internet.password() };
+
+        // @ts-expect-error
+        const promise = sut.signin(request);
+
+        await expect(promise).rejects.toThrow(new ValidationError(AuthRequiredError.EMAIL));
+    });
+});
