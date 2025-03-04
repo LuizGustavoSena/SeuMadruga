48b316aad5f12385017a3557b9d734844b5095d1
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-03 22:49:20 -0300

    fix: adjust MakeTransactionService to MakeTransactionValidation

--- src/main/factories/use-cases/makeTransactionService.ts
+++ src/main/factories/use-cases/makeTransactionService.ts
@@ -1,5 +1,6 @@
 import TransactionService from "@src/data/use-cases/transaction";
 import MakeTransactionDatabase from "../database/specif/makeTransactionDatabase";
+import MakeTransactionValidation from "../validation/transaction";
 
 export default class MakeTransactionService {
     private static instance: TransactionService;
@@ -7,8 +8,11 @@ export default class MakeTransactionService {
     constructor() { };
 
     static getInstance(): TransactionService {
-        if (!MakeTransactionService.instance)
-            MakeTransactionService.instance = new TransactionService(MakeTransactionDatabase.getInstance());
+        if (!MakeTransactionService.instance) {
+            const transactionDatabase = MakeTransactionDatabase.getInstance();
+            const validation = MakeTransactionValidation.getInstance();
+            MakeTransactionService.instance = new TransactionService(transactionDatabase, validation);
+        }
 
         return MakeTransactionService.instance;
     }
