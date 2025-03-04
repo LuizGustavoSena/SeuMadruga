8c411304df364976610a9b0fa4834a28b2bec84d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-04 18:07:14 -0300

    test: adjust makeTransaction to return correct values with type and ammount

--- test/data/mocks/insertTransaction.ts
+++ test/data/mocks/insertTransaction.ts
@@ -2,12 +2,15 @@ import { faker } from "@faker-js/faker/.";
 import { CreateProps, TransactionProps, Type } from "@src/domain/models/transaction";
 
 export const makeTransaction = (params?: Partial<TransactionProps>): CreateProps => {
+    const type = Math.random() < 0.5 ? Type.INPUT : Type.OUTPUT;
+    const ammount = type === Type.INPUT ? faker.number.float() : faker.number.float() * -1;
+
     return {
         acc_id: params?.acc_id ? params.acc_id : faker.number.int(),
-        ammount: params?.ammount ? params.ammount : faker.number.float(),
+        ammount: params?.ammount ? params.ammount : ammount,
         description: params?.description ? params.description : faker.string.sample(),
         status: params?.status ? params.status : Math.random() < 0.5,
-        type: params?.type ? params.type : Math.random() < 0.5 ? Type.INPUT : Type.OUTPUT,
+        type: params?.type ? params.type : type,
         date: params?.date ? params.date : faker.date.past(),
         transfer_id: params?.transfer_id ? params.transfer_id : faker.number.int()
     }
