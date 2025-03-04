ba66c9dbe2529e68b0a9d8720295121228bcf3a0
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-04 17:57:02 -0300

    test: TransactionValidation - Should be erro when create transaction without acc_id

--- test/infrastructure/validation/transactionValidation.test.ts
+++ test/infrastructure/validation/transactionValidation.test.ts
@@ -18,4 +18,15 @@ describe('TransactionValidation', () => {
 
         expect(() => sut.create(transaction)).toThrow(new ValidationError(TransactionRequiredError.DESCRIPTION));
     });
+
+    test('Should be erro when create transaction without acc_id', async () => {
+        const sut = makeSut();
+
+        const transaction = makeTransaction();
+
+        // @ts-expect-error
+        delete transaction.acc_id;
+
+        expect(() => sut.create(transaction)).toThrow(new ValidationError(TransactionRequiredError.ACC_ID));
+    });
 });
