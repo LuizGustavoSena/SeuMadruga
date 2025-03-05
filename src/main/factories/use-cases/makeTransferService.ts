5c5dc51612151a154035a1a9f3e9685237834bda
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-05 18:14:14 -0300

    fix: adjust MakeTransferService class to MakeTransferValidation class

--- src/main/factories/use-cases/makeTransferService.ts
+++ src/main/factories/use-cases/makeTransferService.ts
@@ -1,5 +1,6 @@
 import TransferService from "@src/data/use-cases/transfer";
 import MakeTransferDatabase from "../database/specif/makeTransferDatabase";
+import MakeTransferValidation from "../validation/trasfer";
 
 export default class MakeTransferService {
     private static instance: TransferService;
@@ -7,8 +8,12 @@ export default class MakeTransferService {
     private constructor() { }
 
     static getInstance(): TransferService {
-        if (!MakeTransferService.instance)
-            MakeTransferService.instance = new TransferService(MakeTransferDatabase.getInstance());
+        if (!MakeTransferService.instance) {
+            const transferDatabase = MakeTransferDatabase.getInstance();
+            const validation = MakeTransferValidation.getInstance();
+
+            MakeTransferService.instance = new TransferService(transferDatabase, validation);
+        }
 
         return MakeTransferService.instance;
     }
