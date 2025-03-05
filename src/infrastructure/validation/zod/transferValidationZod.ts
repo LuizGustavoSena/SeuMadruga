62e08962445b5f63961767bc2ee89cc7603b3128
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-05 18:13:30 -0300

    feat: create TransferValidationZod class

---
+++ src/infrastructure/validation/zod/transferValidationZod.ts
@@ -0,0 +1,23 @@
+import { ValidationError } from "@src/domain/error/validationError";
+import { CreateTransfer } from "@src/domain/models/transfer";
+import { TransferMessageError, TransferRequiredError, TransferValidation } from "@src/domain/validations/transfer";
+import { z, ZodError } from "zod";
+
+export default class TransferValidationZod implements TransferValidation {
+    validation = z.object({
+        description: z.string({ required_error: TransferRequiredError.DESCRIPTION }),
+        ammount: z.number({ required_error: TransferRequiredError.AMMOUNT }),
+        acc_ori_id: z.number({ required_error: TransferRequiredError.ACC_ORI_ID }),
+        acc_dest_id: z.number({ required_error: TransferRequiredError.ACC_DEST_ID }),
+    }).refine(data => data.acc_ori_id !== data.acc_dest_id, { message: TransferMessageError.ACC_ORI_ID_EQUAL_ACC_DEST_ID });
+
+    create(params: CreateTransfer): void {
+        try {
+            this.validation.parse(params)
+        } catch (error: any) {
+            if (!(error instanceof ZodError)) return;
+
+            throw new ValidationError(error.errors.map(el => el.message).join(', '))
+        }
+    }
+}
