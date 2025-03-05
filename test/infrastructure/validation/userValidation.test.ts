e23aa694eb89ad7185276624c2af707e74e0cf6c
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-05 17:48:02 -0300

    test: adjust UserValidation's makeSut

--- test/infrastructure/validation/userValidation.test.ts
+++ test/infrastructure/validation/userValidation.test.ts
@@ -1,72 +1,59 @@
 import { faker } from "@faker-js/faker/.";
-import UserService from "@src/data/use-cases/user";
 import { ValidationError } from "@src/domain/error/validationError";
 import { UserMessageError, UserRequiredError } from "@src/domain/validations/user";
 import UserValidationZod from "@src/infrastructure/validation/zod/userValidationZod";
-import DatabaseSpy from "@test/data/mocks/databaseSpy";
-import { EncryptSpy } from "@test/data/mocks/encryptSpy";
 import { makeUser } from "@test/data/mocks/insertUser";
 
-const makeSut = (): UserService => {
-    const sut = new UserService(new DatabaseSpy(), new EncryptSpy(), new UserValidationZod())
+const makeSut = (): UserValidationZod => {
+    const sut = new UserValidationZod();
 
     return sut;
 }
 
 describe('UserValidation', () => {
-    test('Should be error when create user without name', async () => {
+    test('Should be error when create user without name', () => {
         const sut = makeSut();
 
         const request = makeUser();
         // @ts-expect-error
         delete request.name;
 
-        const promise = sut.save(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(UserRequiredError.NAME));
+        expect(() => sut.create(request)).toThrow(new ValidationError(UserRequiredError.NAME));
     });
 
-    test('Should be error when create user without email', async () => {
+    test('Should be error when create user without email', () => {
         const sut = makeSut();
 
         const request = makeUser();
         // @ts-expect-error
         delete request.email;
 
-        const promise = sut.save(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(UserRequiredError.EMAIL));
+        expect(() => sut.create(request)).toThrow(new ValidationError(UserRequiredError.EMAIL));
     });
 
-    test('Should be error when create user without password', async () => {
+    test('Should be error when create user without password', () => {
         const sut = makeSut();
 
         const request = makeUser();
         // @ts-expect-error
         delete request.password;
 
-        const promise = sut.save(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(UserRequiredError.PASSWORD));
+        expect(() => sut.create(request)).toThrow(new ValidationError(UserRequiredError.PASSWORD));
     });
 
-    test('Should be error when create user with wrong email', async () => {
+    test('Should be error when create user with wrong email', () => {
         const sut = makeSut();
 
         const request = makeUser({ email: faker.string.uuid() });
 
-        const promise = sut.save(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(UserMessageError.EMAIL));
+        expect(() => sut.create(request)).toThrow(new ValidationError(UserMessageError.EMAIL));
     });
 
-    test('Should be error when create user with small password', async () => {
+    test('Should be error when create user with small password', () => {
         const sut = makeSut();
 
         const request = makeUser({ password: faker.internet.password().slice(0, 4) });
 
-        const promise = sut.save(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(UserMessageError.PASSWORD));
+        expect(() => sut.create(request)).toThrow(new ValidationError(UserMessageError.PASSWORD));
     });
 });
