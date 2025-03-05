349f0ae86c061bf0577b363d14fe6506006486ac
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-05 17:46:01 -0300

    test: adjust AuthValidation's makeSut

--- test/infrastructure/validation/authValidation.test.ts
+++ test/infrastructure/validation/authValidation.test.ts
@@ -1,66 +1,46 @@
 import { faker } from "@faker-js/faker/.";
-import AuthService from "@src/data/use-cases/auth";
-import UserService from "@src/data/use-cases/user";
 import { ValidationError } from "@src/domain/error/validationError";
 import { AuthMessageError, AuthRequiredError } from "@src/domain/validations/auth";
 import AuthValidationZod from "@src/infrastructure/validation/zod/authValidationZod";
-import UserValidationZod from "@src/infrastructure/validation/zod/userValidationZod";
-import DatabaseSpy from "@test/data/mocks/databaseSpy";
-import MakeEncryptSpy from "@test/data/mocks/encryptSpy";
-import JwtSpy from "@test/data/mocks/jwtSpy";
 
-const makeSut = (): AuthService => {
-    const encrypt = MakeEncryptSpy.getInstance();
-    const sut = new AuthService(
-        new UserService(new DatabaseSpy(), encrypt, new UserValidationZod()),
-        encrypt,
-        new JwtSpy(),
-        new AuthValidationZod())
-        ;
+const makeSut = (): AuthValidationZod => {
+    const sut = new AuthValidationZod();
 
     return sut;
 }
 
 describe('AuthValidation', () => {
-    test('Should be error when signin without email', async () => {
+    test('Should be error when signin without email', () => {
         const sut = makeSut();
 
         const request = { password: faker.internet.password() };
 
         // @ts-expect-error
-        const promise = sut.signin(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(AuthRequiredError.EMAIL));
+        expect(() => sut.signin(request)).toThrow(new ValidationError(AuthRequiredError.EMAIL));
     });
 
-    test('Should be error when signin without password', async () => {
+    test('Should be error when signin without password', () => {
         const sut = makeSut();
 
         const request = { email: faker.internet.email() };
 
         // @ts-expect-error
-        const promise = sut.signin(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(AuthRequiredError.PASSWORD));
+        expect(() => sut.signin(request)).toThrow(new ValidationError(AuthRequiredError.PASSWORD));
     });
 
-    test('Should be error when signin wrong email', async () => {
+    test('Should be error when signin wrong email', () => {
         const sut = makeSut();
 
         const request = { email: faker.string.uuid(), password: faker.internet.password() };
 
-        const promise = sut.signin(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(AuthMessageError.EMAIL));
+        expect(() => sut.signin(request)).toThrow(new ValidationError(AuthMessageError.EMAIL));
     });
 
-    test('Should be error when signin wrong password', async () => {
+    test('Should be error when signin wrong password', () => {
         const sut = makeSut();
 
         const request = { email: faker.internet.email(), password: faker.internet.password().slice(0, 3) };
 
-        const promise = sut.signin(request);
-
-        await expect(promise).rejects.toThrow(new ValidationError(AuthMessageError.PASSWORD));
+        expect(() => sut.signin(request)).toThrow(new ValidationError(AuthMessageError.PASSWORD));
     });
 });
