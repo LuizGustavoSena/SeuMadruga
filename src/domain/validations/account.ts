902b93b702761d9179db7930ffd463a45f96c1f0
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-06 22:01:36 -0300

    feat: create AccountMessageError enum

--- src/domain/validations/account.ts
+++ src/domain/validations/account.ts
@@ -4,6 +4,10 @@ export enum AccountRequiredError {
     NAME = 'Campo name deve ser preenchido'
 }
 
+export enum AccountMessageError {
+    ANOTHER_USERS_ACCOUNT = 'Essa conta não pertence a esse usuário'
+}
+
 export interface AccountValidation {
     create(params: CreateProps): void;
     update(params: UpdateParams): void;
