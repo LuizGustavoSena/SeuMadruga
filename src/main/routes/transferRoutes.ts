ab996b3d490a9e69abb3c0e30f9c7aab97763964
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-09 09:50:25 -0300

    feat: transferRoute

---
+++ src/main/routes/transferRoutes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import MakeTransferController from "../factories/controllers/makeTransferController";
+
+const router = express.Router();
+
+const transferController = MakeTransferController.getInstance();
+
+router.param('id', transferController.paramsInterceptor);
+
+router.route('/')
+    .post(transferController.create)
+    .get(transferController.getAll);
+
+router.route('/:id')
+    .get(transferController.getById)
+    .put(transferController.updateById)
+    .delete(transferController.deleteById);
