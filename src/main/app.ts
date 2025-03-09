42e6b9a06ea00fceb11abbb53355d31bc85d457a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-09 10:41:28 -0300

    fix: bodyParser

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,15 +1,16 @@
-import passport from '@src/main/middlewares/protectedRouter';
+import authenticate from '@src/main/middlewares/protectedRouter';
 import accountRoutes from '@src/main/routes/accountRoutes';
 import authRoutes from '@src/main/routes/authRoutes';
 import balanceRoutes from '@src/main/routes/BalanceRoutes';
 import transactionRoutes from '@src/main/routes/transactionRoutes';
 import transferRoutes from '@src/main/routes/transferRoutes';
+import { json } from 'body-parser';
 import express from 'express';
 
 const app = express();
 const protectedRouter = express.Router();
 
-app.use(express.json());
+app.use(json());
 
 app.use('/auth', authRoutes);
 
@@ -18,6 +19,6 @@ protectedRouter.use('/transactions', transactionRoutes);
 protectedRouter.use('/transfers', transferRoutes);
 protectedRouter.use('/balance', balanceRoutes);
 
-app.use('/v1', passport.authenticate(), protectedRouter);
+app.use('/v1', authenticate(), protectedRouter);
 
 export default app;
