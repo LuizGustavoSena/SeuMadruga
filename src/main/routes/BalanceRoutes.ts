46d69280f02ab75285fd02da664c8ff8de555745
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-08 22:36:45 -0300

    feat: create BalanceRoute

---
+++ src/main/routes/BalanceRoutes.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import MakeBalanceController from "../factories/controllers/makeBalanceController";
+
+const router = express.Router();
+
+const balanceController = MakeBalanceController.getInstance();
+
+router.get('/', balanceController.getBalance);
