6f722f246fd2025c82c8b4139ea789dc628f49e5
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-09 10:07:06 -0300

    feat: delete consign dependency files

--- src/config/middlewares.ts
+++
@@ -1,4 +0,0 @@
-import bodyParser from "body-parser";
-import { Express } from 'express';
-
-module.exports = (app: Express) => app.use(bodyParser.json());
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv';
-import app from './app';
-
-const port = process.env.port || 3001;
-
-app.listen(port, () => console.log(`Server is running in port ${port}`));
