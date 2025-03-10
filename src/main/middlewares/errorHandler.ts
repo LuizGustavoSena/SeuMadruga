72b997c4d2988bed754ab741d1ad5f2ab5f0e59a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-10 18:00:00 -0300

    feat: errorHandler function

---
+++ src/main/middlewares/errorHandler.ts
@@ -0,0 +1,13 @@
+import { ValidationError } from '@src/domain/error/validationError';
+import { Response } from 'express';
+
+const errorHandler = (err: Error, res: Response) => {
+    if (err instanceof ValidationError) {
+        return res.status(400).json({ error: err.message });
+    }
+
+    return res.status(500).json({ error: "Internal server error" });
+};
+
+export { errorHandler };
+
