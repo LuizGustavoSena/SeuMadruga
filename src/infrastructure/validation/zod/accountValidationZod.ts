3b252e9ab5dbd25cd2f9bd81465b57032f3532d3
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-11 22:20:30 -0300

    feat: method id in AccountValidationZod class

--- src/infrastructure/validation/zod/accountValidationZod.ts
+++ src/infrastructure/validation/zod/accountValidationZod.ts
@@ -1,12 +1,14 @@
 import { ValidationError } from "@src/domain/error/validationError";
 import { CreateProps, UpdateParams } from "@src/domain/models/account";
-import { AccountRequiredError, AccountValidation } from "@src/domain/validations/account";
+import { AccountMessageError, AccountRequiredError, AccountValidation } from "@src/domain/validations/account";
 import { z, ZodError } from "zod";
 
 export default class AccountValidationZod implements AccountValidation {
     validation = z.object({
         name: z.string({ required_error: AccountRequiredError.NAME })
     });
+
+    validationId = z.number({ message: AccountMessageError.ID });
 
     constructor() { }
 
@@ -22,7 +24,17 @@ export default class AccountValidationZod implements AccountValidation {
 
     update(params: UpdateParams): void {
         try {
-            this.validation.parse(params)
+            this.validation.parse(params);
+        } catch (error: any) {
+            if (!(error instanceof ZodError)) return;
+
+            throw new ValidationError(error.errors.map(el => el.message).join(', '))
+        }
+    }
+
+    id(id: number): void {
+        try {
+            this.validationId.parse(id);
         } catch (error: any) {
             if (!(error instanceof ZodError)) return;
 
