4cc3dce8577d9de9d9f4bac09ca894bdac1e9fc2
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-12 18:14:13 -0300

    feat: messageError transfer

--- src/domain/validations/transfer.ts
+++ src/domain/validations/transfer.ts
@@ -8,9 +8,13 @@ export enum TransferRequiredError {
 }
 
 export enum TransferMessageError {
-    ACC_ORI_ID_EQUAL_ACC_DEST_ID = 'acc_ori_id e acc_dest_id devem ter valores diferentes'
+    ACC_ORI_ID_EQUAL_ACC_DEST_ID = 'acc_ori_id e acc_dest_id devem ter valores diferentes',
+    ACC_ORI_ID_ANOTHER_USER = 'acc_ori_id não pertence a esse usuário',
+    ACC_DEST_ID_ANOTHER_USER = 'acc_dest_id não pertence a esse usuário',
+    ID = 'Valor de parametro deve ser um number',
 }
 
 export interface TransferValidation {
     create(params: CreateTransfer): void;
+    id(id: number): void;
 }
