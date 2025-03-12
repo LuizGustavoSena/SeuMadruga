58078d24217fcb0a5f67f3eef6fe4173a7577d8d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-12 18:02:05 -0300

    feat: message error another accounts transaction

--- src/domain/validations/transaction.ts
+++ src/domain/validations/transaction.ts
@@ -10,9 +10,12 @@ export enum TransactionRequiredError {
 export enum TransactionMessageError {
     POSITIVE_AMMOUNT = 'Campo ammount deve ser positivo',
     NEGATIVE_AMMOUNT = 'Campo ammount deve ser negativo',
-    TYPE = 'Campo type inválido'
+    TYPE = 'Campo type inválido',
+    ID = 'Valor de parametro deve ser um number',
+    ANOTHER_ACCOUNT_TRANSACTION = 'Essa transação não pertence a essa conta',
 }
 
 export interface TransactionValidation {
     create(params: CreateProps): void;
+    id(id: number): void;
 }
