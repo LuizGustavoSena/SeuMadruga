7aa8e226ac8216b33b9f1138f732a2de6191cee4
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-12 18:14:23 -0300

    feat: method id validation transfer

--- src/infrastructure/validation/zod/transferValidationZod.ts
+++ src/infrastructure/validation/zod/transferValidationZod.ts
@@ -11,9 +11,21 @@ export default class TransferValidationZod implements TransferValidation {
         acc_dest_id: z.number({ required_error: TransferRequiredError.ACC_DEST_ID }),
     }).refine(data => data.acc_ori_id !== data.acc_dest_id, { message: TransferMessageError.ACC_ORI_ID_EQUAL_ACC_DEST_ID });
 
+    validationId = z.number({ message: TransferMessageError.ID });
+
     create(params: CreateTransfer): void {
         try {
-            this.validation.parse(params)
+            this.validation.parse(params);
+        } catch (error: any) {
+            if (!(error instanceof ZodError)) return;
+
+            throw new ValidationError(error.errors.map(el => el.message).join(', '))
+        }
+    }
+
+    id(id: number): void {
+        try {
+            this.validationId.parse(id);
         } catch (error: any) {
             if (!(error instanceof ZodError)) return;
 
