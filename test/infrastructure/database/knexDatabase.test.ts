83e8546e6b1c54ea276b324548626c0912c4feb0
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-16 17:26:01 -0300

    test: KnexDatabase - Should be successful delete object

--- test/infrastructure/database/knexDatabase.test.ts
+++ test/infrastructure/database/knexDatabase.test.ts
@@ -1,14 +1,18 @@
+import { faker } from "@faker-js/faker/.";
 import { FindByEmailResponse, UserProps } from "@src/domain/models/user";
 import KnexDatabase from "@src/infrastructure/database/knex";
 import { makeUser } from "@test/data/mocks/insertUser";
 
 const sut: KnexDatabase = new KnexDatabase('users');
-const auxiliarUser = makeUser();
 
 describe('KnexDatabase', () => {
+    let auxiliarUser: UserProps & { id?: number } = makeUser();
+
     beforeAll(async () => {
         await sut.db.migrate.latest();
-        await sut.create<UserProps, FindByEmailResponse>(auxiliarUser);
+        const response = await sut.create<UserProps, FindByEmailResponse>(auxiliarUser);
+
+        auxiliarUser.id = response.id;
     });
 
     afterAll(async () => {
@@ -40,5 +44,27 @@ describe('KnexDatabase', () => {
         expect(response[0].email).toBe(auxiliarUser.email);
         expect(response[0].name).toBe(auxiliarUser.name);
         expect(response[0].password).toBe(auxiliarUser.password);
+    });
+
+    test('Should be successful update object', async () => {
+        const newName = faker.person.firstName();
+
+        const response = await sut.update<FindByEmailResponse>({
+            id: auxiliarUser.id ?? 0,
+            data: { name: newName }
+        });
+
+        expect(response.id).toBe(auxiliarUser.id);
+        expect(response.name).toBe(newName);
+        expect(response.email).toBe(auxiliarUser.email);
+        expect(response.password).toBe(auxiliarUser.password);
+    });
+
+    test('Should be successful delete object', async () => {
+        await sut.deleteById(auxiliarUser.id ?? 0);
+
+        const response = await sut.getByFIlter<FindByEmailResponse[]>({ name: auxiliarUser.name });
+
+        expect(response).toHaveLength(0);
     });
 });
