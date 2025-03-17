6d640b4bafd17a9b15dad332efbbc620232d5051
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-17 19:46:18 -0300

    test: BalanceKnexDatabase - Should be successful balance

--- test/infrastructure/database/specific/balanceKnexDatabase.test.ts
+++ test/infrastructure/database/specific/balanceKnexDatabase.test.ts
@@ -1,6 +1,9 @@
+import { faker } from "@faker-js/faker/.";
 import { CreateProps } from "@src/domain/models/account";
+import { Type } from "@src/domain/models/transaction";
 import BalanceKnexDatabase from "@src/infrastructure/database/specific/balanceKnex";
 import { makeAccount } from "@test/data/mocks/insertAccount";
+import { makeTransaction } from "@test/data/mocks/insertTransaction";
 
 const sut: BalanceKnexDatabase = new BalanceKnexDatabase();
 
@@ -22,5 +25,31 @@ describe('BalanceKnexDatabase', () => {
         const response = await sut.getBalanceByUserId(auxiliarAccount.user_id);
 
         expect(response).toHaveLength(0);
+    });
+
+    test('Should be successful balance', async () => {
+        const transaction = makeTransaction({
+            acc_id: auxiliarAccount.id,
+            date: faker.date.past(),
+            type: Type.INPUT,
+            ammount: faker.number.float(),
+            status: true
+        });
+
+        const transaction2 = makeTransaction({
+            acc_id: auxiliarAccount.id,
+            date: faker.date.past(),
+            type: Type.INPUT,
+            ammount: faker.number.float(),
+            status: true
+        });
+
+        await sut.db('transactions').insert(transaction);
+        await sut.db('transactions').insert(transaction2);
+
+        const response = await sut.getBalanceByUserId(auxiliarAccount.user_id);
+
+        expect(response).toHaveLength(1);
+        expect(response[0].sum).toBe(transaction.ammount + transaction2.ammount);
     });
 });
