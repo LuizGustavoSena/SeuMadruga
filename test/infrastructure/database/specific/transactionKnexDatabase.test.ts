de4ef8e5d15f0ceef6853f18648bad5ee8945d3a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-18 17:54:01 -0300

    test: TransactionKnexDatabase - Should be successful transactions

--- test/infrastructure/database/specific/transactionKnexDatabase.test.ts
+++ test/infrastructure/database/specific/transactionKnexDatabase.test.ts
@@ -1,6 +1,7 @@
 import { CreateProps } from "@src/domain/models/account";
 import TransactionKnexDatabase from "@src/infrastructure/database/specific/transactionKnex";
 import { makeAccount } from "@test/data/mocks/insertAccount";
+import { makeTransaction } from "@test/data/mocks/insertTransaction";
 
 const sut: TransactionKnexDatabase = new TransactionKnexDatabase();
 
@@ -22,5 +23,14 @@ describe('TransactionKnexDatabase', () => {
         const response = await sut.findTransactions({ user_id: auxiliarAccount.user_id });
 
         expect(response).toHaveLength(0);
+    });
+
+    test('Should be successful transactions', async () => {
+        const transaction = makeTransaction({ acc_id: auxiliarAccount.id });
+        await sut.db('transactions').insert(transaction);
+
+        const response = await sut.findTransactions({ user_id: auxiliarAccount.user_id });
+
+        expect(response).toHaveLength(1);
     });
 });
